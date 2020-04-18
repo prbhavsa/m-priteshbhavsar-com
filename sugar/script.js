b34b4816e70bcedc5648d83ec239d20396a4dc81
Robot Solution <manage.ars1@gmail.com>
2020-04-17 22:02:02 -0400

    update

--- sugar/script.js
+++ sugar/script.js
@@ -234,8 +234,39 @@ $(document).ready(function() {
                     "No one knows how tough the LongDistance relationship is and we both have learned it hard way the secret to grow in long distance. The key is to be truthful and having no shame in sharing even worst of incident or thing happened with each other. We cannot touch each other or feel the smell of each other.. but the only way we feel each other is by staying truthful and loyal. Numerous times you have broken down in front of me let it be family or personal issue, you have always been transparent with me. I may not be this transparent because I care too much about u, but for most of its part I do share everything thing with u. I think this is the only key to be in long distance. Just like a very long challenge that we have taken on each other. I love u for supporting this unusual love adventure we have.\n" +
                     "<h2>10) I love that your hand fits perfectly with mine.</h2>" +
                     "If possible listen to “Vanilla Twilight” after reading this, the line in this song “Cause the spaces between my fingers Are right where yours fit perfectly”. I may have imagined this for 3 years, looked forward for 1 year more, and when we met for the first time and we hold hand with your hands fitting in my hands… I had this goosebumps which I could not explain. Even in this trip when u were holding my hand and sleeping with it… the feeling of u with me was amazing and I just kept looking at u while u were sleeping. Hugging u makes me feel amazing.. but when your hand fits in mine, it has its own beauty and feeling. ";
+			case '2':
+				return "<h2> 11) You always keep your promises </h2> " +
+                    "I may be lazy to keep my promises and not that punctual to actually keep it in mind, but in contrast you always keep your promises… like almost always.. now that I am thinking even I keep most of my promises. Now this point reminds me of the promos I made to you while I was getting in the airport “Please wait for me to come back and marry you.. I promise”. You promised me that you will wait and I am sure u will keep this promise. <3\n" +
+
+                    "<h2> 12) You tell me why you love me and hate me </h2> " +
+                    "Most important thing in a relationship is to be transparent in every aspect, and u exactly do that. The letter which u have written for me are very import as they explain me exactly how u love me… No, I am not suggesting this after reading all the letters, but the gesture of writing the letters for me itself shows me how much u love me unconditionally. You have said “I Hate You” numerous times, but its ok.. u can hate a person whom u love… so that way I know I must have hurt u to hate me.. \n" +
+
+                    "<h2> 13) I love that you take care of me when I forget to </h2> " +
+                    "I know I am all over the place and very workaholic. Many times I forgot to take care of myself like forgot to have breakfast or lunch, trimming beards, messaging u or mom dad.. but u always yell at me and remind me to take care of myself and when u are very annoyed u send the song “Fursat” or keep it as you WhatsApp story to make me realize. I love u for taking care of me even being 12k mies apart.\n" +
+
+                    "<h2> 14) I love u because you gave me the gift of yourself. </h2> " +
+                    "I love u because of the incredible life that u and I have built together. Every memory, step, and the journey taken with u means so much to me and all of it would not have the same meaning if u were not a part of it.\n" +
+
+                    "<h2> 15) I love u because you support my dreams. </h2> " +
+                    "I love u because u have always been so supportive of me and my dreams in ways that I could not have imagined. It was my dream to pursue masters in USA, you supported me even when we were not in relationship. Now that we are in relationship you are always there to give me confidence to anything and everything I am planning… just like when I was in a dilemma about PHD, you supported me and showed me right direction and helped me to take decision.";
+			case '3':
+				return "<h2> 16) Because I miss you </h2> " +
+                    "We being separated by distance is a tough thing as you already know. I have seen so many relationships breaking down because of this distance. But I think there is something we have for each other that make us Miss each other. No matter what I cannot imagine anyone else coz I miss u in everything. I just can’t imagine U & I… for me it is US for eternity.\n" +
+
+                    "<h2> 17) I love you because I bore my friends with your topic.</h2> " +
+                    "Now this is something which I have reduced doing now, but I don’t know my brain somehow or other relates any topic with the story between u and me. Funny enough we have only spent 5 days physically together, yet I do not know how I can relate almost every story with u. I use bore my friends with you topic so many times.\n" +
+
+                    "<h2> 18) You love my family, my family loves you. </h2> " +
+                    "I have never seen my parents being so liberal and confident on me for a girl, but u have sprinkled perfect magic on my parents especially on my mom cos she does love u and have already seen you as her daughter in law. And I am sure this is because u also love my parents equally. My mom has never talked to any of my girl friend (like general friend) with so much comfort, she always use to get very protective about me and use to tell me to stay away for that girl. But for the first time she is positive about a girl and that girl is u. :*\n" +
+
+                    "<h2>  19) I love u for the idea of getting old with you </h2> " +
+                    "In numerous chats and texts we have planned our life together, like when to have children, how may, if we should adopt, where to go for honey moon, how should be our house, should we have pet, supporting ridhi with her future animal care projects.. and the list goes on. But the point is, we have planned everything and for every situation. Like it is not just a liner plan, but we have planned backup plans for the backups if something mess-up. While writing this I can recall all the discussions we had.\n" +
+
+                    "<h2> 20) When I am with you we are in our own special little world together where nobody else exists. </h2> " +
+                    "With the very little physical time we spent, especially the most recent trip of 2 days, the amount of time was very less and the situation was very difficult for both of us with roits and then change of hotel. But I was not focused on any of it, for me Important was that we both were stuck together in this situation and we don’t have anywhere to go. Ridhi was with us but she was also part of our little world together. Twice I watched u sleeping, u looked like a little babu.. and I didn’t wanted to disturb ur sleep. I just got lost into u, while hugging u. For that time my world was just u, and I want out future little world to start soon.\n";
+
 			default:
-				return "I can only give 10 reasons a day. Coz excitement bana rehna chayeea... :P <br> <h2>Listen to \"Ek Mai Hu Aur Ek Tu \" Song</h2>";
+				return "I can only give 10 reasons a day. Coz excitement bana rehna chayeea... :P <br> <h2>We will watch the move \" The Notebook\" together. "</h2>";
 		}
 	}
 });
